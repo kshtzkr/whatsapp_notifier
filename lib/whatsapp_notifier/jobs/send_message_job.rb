b3027f18a1ad7fb7bd677421eb4f95c018bbd101
kshitiz sinha <kshtzkr@gmail.com>
2026-05-03 16:30:50 +0530

    Refactor: removed unwanted changes

--- lib/whatsapp_notifier/jobs/send_message_job.rb
+++ lib/whatsapp_notifier/jobs/send_message_job.rb
@@ -1,11 +1,28 @@
 module WhatsAppNotifier
   module Jobs
-    class SendMessageJob < ::ActiveJob::Base
-      queue_as :default
+    if defined?(::ActiveJob::Base)
+      class SendMessageJob < ::ActiveJob::Base
+        queue_as :default
 
-      def perform(notification_class_name, params)
-        klass = notification_class_name.split("::").inject(Object) { |ctx, const| ctx.const_get(const) }
-        klass.with(params).deliver_now
+        def perform(notification_class_name, params)
+          klass = notification_class_name.split("::").inject(Object) { |ctx, const| ctx.const_get(const) }
+          klass.with(params).deliver_now
+        end
+      end
+    else
+      class SendMessageJob
+        def self.perform_later(notification_class_name, params)
+          perform_now(notification_class_name, params)
+        end
+
+        def self.perform_now(notification_class_name, params)
+          new.perform(notification_class_name, params)
+        end
+
+        def perform(notification_class_name, params)
+          klass = notification_class_name.split("::").inject(Object) { |ctx, const| ctx.const_get(const) }
+          klass.with(params).deliver_now
+        end
       end
     end
   end
